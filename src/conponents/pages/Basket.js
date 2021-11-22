c283d77bb25b61383d1193bcaa24a61f88dd2d97
ogundemirkiran <demirkiranogun@gmail.com>
2021-11-22 21:44:17 +0300

    update files

--- src/conponents/pages/Basket.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import "../../App.css"
-export default function Basket() {
-  return (
-    <>
-      <h1 className='basket'></h1>
-    </>
-  );
-}
---
+++ src/pages/Basket/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../App.css";
+
+export default function Basket() {
+  return (
+    <>
+      <h1 className="basket"></h1>
+    </>
+  );
+}
