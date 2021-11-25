704ff42dbd0923ea5243ae4865a872842f6fadad
Mehmet Batuhan Yılmaz <84319499+mehmetbatuhanyilmaz@users.noreply.github.com>
2021-11-25 19:13:12 +0300

    .

--- src/pages/ContactUs/index.js
+++ src/pages/ContactUs/index.js
@@ -10,12 +10,12 @@ function Contact() {
   return (
     <>
     
-   <div>
+   <div style={{backgroundImage: "url(images/iletisim.jpeg)"}}>
     <div className="contactdiv">
       
       <div className="contactinfodiv">
         <h2>Contact Us</h2>
-        <p> Kraft.co Clothes and Formal Wear company was developed in 2021 by Front End Developer M. Batuhan Yılmaz and Ogün Demirkıran with their contributions Kodluyoruz ve Patika.dev , using React Js. ☺ </p>
+        <p> Kraft.co Clothes and Formal Wear company was developed in 2021 by Front End Developer M. Batuhan Yılmaz and Ogün Demirkıran with their contributions Kodluyoruz and Patika.dev , using React Js. ☺ </p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
         
       </div>
